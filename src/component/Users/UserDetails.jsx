26c376e79c3a64217fb8ac05e3825ff00a3fd1d4
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-17 00:41:43 +0600

    modified user details card and added users data

--- src/component/Users/UserDetails.jsx
+++ src/component/Users/UserDetails.jsx
@@ -7,18 +7,30 @@ const UserDetails = () => {
 
     console.log(data)
 
-    const {name,email,username,address,phone,website,company} = data;
+    const { name, email, username, address, phone, website, company } = data;
+    
+    const {street,suite,city,zipcode} = address;
 
     return (
-        <div className='border-2 border-blue-400 rounded-xl p-6'>
-            <h3 className='text-center text-xl font-medium'>{name}</h3>
-            <div>
-                <p>@Username : {username}</p>
-                <p>Email : {email}</p>
-                <p>Website : {website}</p>
-                <p>Phone : {phone}</p>
+
+        <div className=' border-2 border-blue-400 rounded-xl py-6 ml-72 w-md mt-24'>
+            <h3 className='text-center text-xl font-medium pb-4'>{name}</h3>
+            <div className='border-t-2 border-blue-400 rounded-xl p-6'>
+                <p className='py-2'>Username : @{username}</p>
+                <p className=''>Email : {email}</p>
+                <p className='py-2'>Website : {website}</p>
+                <p className=''>Phone : {phone}</p>
+                <p className='py-2'>Company : {company.name} </p>
+                <p className='pb-2 text-md font-medium'>Address :</p>
+                <ul>
+                    <li className='ml-7'>Street : {street}</li>
+                    <li className='ml-7'>Suite : {suite}</li>
+                    <li className='ml-7'>City : {city}</li>
+                    <li className='ml-7'>Zipcode : {zipcode}</li>
+                </ul>
             </div>
         </div>
+
     );
 };
 
